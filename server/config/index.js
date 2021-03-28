664d5543b440c5bd3bb4b5ee7d171c91a753aa79
Chi-Quyen Le <quyen.lechi@gmail.com>
2021-03-27 17:40:00 -0700

    feat: set up cors

--- server/config/index.js
+++ server/config/index.js
@@ -2,4 +2,5 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8000,
   dbConnectionString: process.env.DB_CONNECTION_STRING,
+  corsDomain: process.env.CORS_DOMAIN,
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+const corsOptions = config.corsDomain && {
+  origin: config.corsDomain,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use('/api', require('./routes'));
 
