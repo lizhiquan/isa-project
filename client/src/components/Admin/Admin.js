9b5752bbc85326c429c63566ef1beb88bd39f07e
Shane F <shanevi29@gmail.com>
2021-04-10 21:11:50 -0700

    cleanup

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { createCourse } from '../../api/apiService';
 import CreateCourse from '../Course/CreateCourse';
